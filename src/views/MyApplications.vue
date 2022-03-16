acc3403cce80d55bfb4d934e6497f47b5599b8a1
yuxiankok <yuxian.kok@dsanalytics.com>
2022-03-17 02:29:18 +0800

    removed '.' under div maintext to refine the display

--- src/views/MyApplications.vue
+++ src/views/MyApplications.vue
@@ -10,7 +10,7 @@
             font-family: Georgia, 'Times New Roman', Times, serif;
           "
         >
-          My Applications.
+          My Applications
         </div>
         <p style="color: black; text-align: center">
           “As you grow older, you will discover that you have two hands <br />
