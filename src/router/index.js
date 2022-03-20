e7ab0bf96c898d4c30b61592958d9a838ff261fe
Su Zifeng <suzifeng1@gmail.com>
2022-03-20 17:16:30 +0800

    Revert "Second commit of Reward"
    This reverts commit 25c2136abb021ef64a1ea7d466f3620f91bd2b93.

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Volunteer from "@/views/Volunteer.vue";
 import MyApplications from "@/views/MyApplications.vue";
-import Reward from "@/views/Reward.vue";
-
 
 const routes = [
   {
@@ -21,11 +19,6 @@ const routes = [
     path: "/volunteer",
     name: "Volunteer",
     component: Volunteer,
-  },
-  {
-    path: "/reward",
-    name: "Reward",
-    component: Reward,
   },
   {
     path: "/myapplications",
