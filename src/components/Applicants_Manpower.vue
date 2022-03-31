28f74609369b76c9381e7f934da95e15c7c8df5d
Su Zifeng <suzifeng1@gmail.com>
2022-03-31 18:21:21 +0800

    Prototype of applicants/manpower  page

--- src/components/Applicants_Manpower.vue
+++ src/components/Applicants_Manpower.vue
@@ -36,7 +36,122 @@
 </template>
 
 <script>
-export default {};
+import firebaseApp from "../firebase.js";
+import { getFirestore } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  updateDoc,
+  deleteDoc,
+  increment,
+} from "firebase/firestore";
+export default {
+  mounted() {
+    const db = getFirestore(firebaseApp);
+    async function display() {
+      let z = await getDocs(collection(db, "Applicants(Zifeng)"));
+      let ind = 1;
+
+      z.forEach((docs) => {
+        let data = docs.data();
+        var table = document.getElementById("table");
+
+        var row = table.insertRow(ind);
+        var date = docs.get("Applied Date");
+        var name = data.Name;
+        var listing_name = data.Listing;
+        var status = data.Status;
+
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        var cell6 = row.insertCell(5);
+        var cell7 = row.insertCell(6);
+
+        cell1.innerHTML = name;
+        cell2.innerHTML = listing_name;
+        cell3.innerHTML = date;
+        cell4.innerHTML = status;
+        cell5.className = "review";
+        cell6.className = "accept";
+        cell7.innerHTML = "reject";
+
+        var review_button = document.createElement("button");
+        review_button.className = "bwt1";
+        review_button.innerHTML = "Review";
+        review_button.onclick = function () {
+          console.log("Review functionality not available yet");
+        };
+        cell5.appendChild(review_button);
+
+        if (status == "Pending") {
+          var accept_button = document.createElement("button");
+          var reject_button = document.createElement("button");
+          accept_button.className = "bwt2";
+          accept_button.innerHTML = "Accept";
+          reject_button.className = "bwt3";
+          reject_button.innerHTML = "Reject";
+          accept_button.onclick = function () {
+            accept(name, listing_name);
+          };
+          reject_button.onclick = function () {
+            reject(name, listing_name);
+          };
+          cell6.appendChild(accept_button);
+          cell7.appendChild(reject_button);
+        }
+
+        if (status == "Approved") {
+          var cancel_button = document.createElement("button");
+          cancel_button.className = "bwt3";
+          cancel_button.innerHTML = "Cancel";
+          cancel_button.onclick = function () {
+            cancel(name, listing_name);
+          };
+          cell7.appendChild(cancel_button);
+        }
+      });
+    }
+    display();
+
+    async function accept(name, listing_name) {
+      // Part 1: Change applicant status to be approved
+      await updateDoc(doc(db, "Applicants(Zifeng)", name), {
+        Status: "Approved",
+      });
+      // Part 2: Change listing volunteer values
+      await updateDoc(doc(db, "Listings", listing_name), {
+        Approved: increment(1),
+        Pending: increment(-1),
+      });
+      display();
+    }
+
+    async function cancel(name, listing_name) {
+      //Part 1: Delete document from applicants
+      await deleteDoc(doc(db, "Applicants(Zifeng", name));
+      //Part 2: Update documents in listings
+      await updateDoc(doc(db, "Listings", listing_name), {
+        Approved: increment(-1),
+        Remaining: increment(1),
+      });
+      display();
+    }
+
+    async function reject(name, listing_name) {
+      //Part 1: Delete document from applicants
+      await deleteDoc(doc(db, "Applicants(Zifeng", name));
+      //Part 2: Update documents in listings
+      await updateDoc(doc(db, "Listings", listing_name), {
+        Pending: increment(-1),
+      });
+      display();
+    }
+  },
+};
 /**
  * Function logic: 2 collections, one for applications and one for listings
  * Each document in application has applicant name, listing name, application date, status
@@ -91,5 +206,17 @@ td {
   border: 1px solid #dddddd;
   text-align: center;
   padding: 8px;
+}
+.bwt1 {
+  background-color: #ff9213;
+  color: white;
+}
+.bwt2 {
+  background-color: #099e21;
+  color: white;
+}
+.bwt3 {
+  background-color: #e81515;
+  color: white;
 }
 </style>
